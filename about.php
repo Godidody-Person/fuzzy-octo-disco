26db8669dae4e59320f3efa714a168590c71d27f
Godidody-Person <bigmushroomfungus@gmail.com>
2025-06-16 11:41:05 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About | LegalBridge</title>
+
+    <link rel="stylesheet" href="stylesheets/default.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+</head>
+<body>
+    <?php
+    include("../src/navigation/navbar.php");
+    ?>
+    <div class="section header">
+        <div>
+            <h1 class="header">About Us</h1>
+        </div>
+        <div class="no-zebra-striping">
+            <div style="display:none"></div>
+
+            <div id="d1">
+                <h1 gradient>2000+</h1>
+                <p>Worldwide clients</p>
+            </div>
+            <div>
+                <h1 gradient>10+</h1>
+                <p>Years of excellence</p>
+            </div>
+            <div>
+                <h1 gradient>10+</h1>
+                <p>Team members</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="transition"></div>
+
+    <div class="section header">
+        <div class="no-zebra-striping">
+            <h1 class="header">Meet the Team</h1>
+            <div>
+                <p>Our team of experienced lawyers is dedicated to providing the highest quality legal services to our clients. We are committed to achieving the best possible outcome for each and every one of our clients.</p>
+            </div>
+        </div>
+
+        <div class="about">
+            <div style="outline:20px solid rgba(241, 238, 255, 0.5);" class="panel-about">
+                <h1>
+                    NingSi Mei
+                </h1>
+
+                <h2>
+                    Lawyer & Co-Founder
+                </h2>
+
+                <img src="media/l1.jpg" class="about-image" id="img1">
+
+                <details>
+                    <summary>About Ningsi Mei</summary>
+                    <div><p>Ningsi Mei is a co-founder of the law firm. She has many years of experience in handling immigration and civil cases, and is proficient in English, French and Chinese (Mandarin). Prior to her law degree from the University of Montreal, she earned a bachelor's degree from McGill University and a master's degree from Queen's University. Her background in science helped her to develop strong logical thinking and analytical skills while maintaining a consistently rigorous and serious attitude when handling legal cases. She became a member of the Barreau du Quebec in 2019, having accumulated a wealth of work experience during her law school years, participating in the assistance of over a hundred real cases, particularly complex issues related to immigration, including the famous interview rejection cases for CSQ applications under the PEQ program. After passing the Quebec bar exam, she also worked as an articling student under the guidance of renowned business law experts to specialize in various related fields of commercial law. She collaborates with numerous lawyers, notaries, and immigration consultants within the industry, providing business consultation and answers to peers. She is committed to offering high-quality legal services to help clients resolve complex issues related to immigration law.</p>
+                    </div>
+                </details>
+            </div>
+            <div style="outline:20px solid rgba(241, 238, 255, 0.5);" class="panel-about" id="d1">
+                <h1>
+                XiaoJing Yang
+                </h1>
+
+                <h2>
+                Lawyer & Co-Founder
+                </h2>
+
+                <img src="media/l2.jpg" class="about-image">
+
+                <details>
+                    <summary>About Xiaojing Yang</summary>
+                    <div>
+                    <p>
+                    Xiaojing Yang is a co-founder of the law firm. She specializes in Canadian immigration law corporate law and commercial law. She serves a client base comprised mainly of SMBs based in Quebec and abroad. She is often called to provide legal services to companies doing businesses both in Canada and in China. She also provides counsels to clients regarding their temporary or permanent establishment in Canada. Having practiced in the legal profession for more than 10 years, Ms. Yang realized that in addition to asserting her clients’ legal rights, she could also be part of the solution in resolving their conflicts. She is an accredited mediator in Civil and Commercial Mediation. She is also completing her accreditation in Family Mediation. Her goal is to help individuals and businesses resolve their conflicts in the shortest possible time and in a cost-effective manner. This means even before the commencement of any legal proceedings to find alternative ways to resolve disputes at hand. Prior to obtaining her Bachelor of Law (LL.B.) degree from Sherbrooke University, Ms. Yang obtained a Bachelor of Science degree from McGill University. Her scientific background helped to develop her logical and analytical spirit when dealing with her legal files. She became a member of the Barreau du Quebec in 2011. She is also a member of the Quebec Association of Immigration Lawyers (AQAADI). Ms. Yang is fluent in French, English and Chinese (Mandarin).
+                    </p>
+                    </div>
+                </details>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
